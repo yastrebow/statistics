0cb6abd91414ba861bdbf08db7589e24d02d8fad
yastrebow <yastrebow@gmail.com>
2022-07-07 09:45:53 +0300

    Initial MS 'statistics'

---
+++ src/test/java/ru/yastrebov/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
